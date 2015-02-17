c7c6b80082f1febdc33cd07539c4c355c2d4585c
Klaus Hebsgaard <klaus@hebsgaard.dk>
2015-02-17 11:43:43 +0100

    ensure user logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :ensure_user_authenticated!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def current_user
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def ensure_user_authenticated!
+    if current_user.nil?
+      redirect_to :new_user_session_path
+    end
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
+  before_filter :ensure_user_authenticated!, :except => [:index]
   def index
     @user = User.first
+  end
+  
+  def show
   end
 end
